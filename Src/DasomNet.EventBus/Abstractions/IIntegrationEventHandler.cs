87d7ce088061a35a79651ae917963304fab763d1
Henry Oh <hodude_99@yahoo.com>
2019-12-31 00:16:58 -0800

    init

---
+++ Src/DasomNet.EventBus/Abstractions/IIntegrationEventHandler.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Threading.Tasks;
+using DasomNet.EventBus.Events;
+
+namespace DasomNet.EventBus.Abstractions
+{
+    public interface IIntegrationEventHandler<in TIntegrationEvent> : IIntegrationEventHandler
+        where TIntegrationEvent : IntegrationEvent
+    {
+        Task Handle(TIntegrationEvent @event);
+    }
+
+    public interface IIntegrationEventHandler
+    {
+    }
+}
